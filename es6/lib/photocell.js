532293cdc1de350824504d764765ee6ef19a9f66
Mike Green <mike.is.green@gmail.com>
2016-12-28 14:11:36 -0500

    Add asynchronous photocell measurement method

--- es6/lib/photocell.js
+++ es6/lib/photocell.js
@@ -1,14 +1,35 @@
 import _                           from 'lodash';
 import wpi, { INPUT, OUTPUT, LOW } from 'wiring-pi';
 import EventEmitter                from 'events';
+import NanoTimer                   from 'nanotimer';
 
 export class Photocell extends EventEmitter {
   constructor(pin, options = {}) {
     super();
     this.pin = pin;
     this.options = _.assign({}, Photocell.defaults, options);
+    this.timer = new NanoTimer();
 
     wpi.setup('gpio');
+  }
+
+  measure() {
+    return new Promise((resolve) => {
+      wpi.pinMode(this.pin, OUTPUT);
+      wpi.digitalWrite(this.pin, LOW);
+
+      this.timer.setTimeout(() => {
+        let reading = 0;
+
+        wpi.pinMode(this.pin, INPUT);
+
+        while (wpi.digitalRead(this.pin) === LOW) {
+          reading++;
+        }
+
+        resolve(reading);
+      }, '100m');
+    });
   }
 
   measureSync() {
@@ -27,9 +48,6 @@ export class Photocell extends EventEmitter {
   }
 }
 
-Photocell.defaults = {
-  timerInterval: 1000,
-  timerUnit: 'u'
-};
+Photocell.defaults = {};
 
 export default Photocell;
